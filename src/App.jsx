2a1ea1346ddb93914a702cf4cc36c56f78c171b5
unknown <crossfireelite601@gmail.com>
2023-04-12 20:07:10 -0300

    add intro netflix

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ export default () => {
   const [movieList, setMovieList] = useState([]);
   const [featuredData, setFeaturedData] = useState(null);
   const [blackHeader, setBlackHeader] = useState(false);
+  const [introNetflix, setIntroNetflix] = useState("loading");
 
   useEffect(() => {
     const loadAll = async () => {
@@ -41,28 +42,57 @@ export default () => {
     };
   }, []);
 
+  useEffect(() => {
+    if (movieList.length < 1) {
+      setIntroNetflix("loading");
+    } else if (movieList.length >= 1) {
+      setTimeout(() => {
+        setIntroNetflix("intro");
+      }, 2800);
+
+      setTimeout(() => {
+        setIntroNetflix("disable");
+      }, 5650);
+    }
+  }, [movieList]);
+
+  console.log(introNetflix);
   return (
-    <div className="page">
-      <Header black={blackHeader} />
-
-      {featuredData && <FeaturedMovie item={featuredData} />}
-      <section className="lists">
-        {movieList.map((item, key) => (
-          <MovieRow key={key} title={item.title} items={item.items} />
-        ))}
-      </section>
-
-      <footer>
-        Feito por Maycon
-        <br /> Direitos de imagem para Netflix Dados pegos do site
-        <br />
-        Themoviedb.org
-      </footer>
-      {movieList.length <= 0 && (
-        <div className="loading">
-          <img src="https://media.filmelier.com/noticias/br/2020/03/Netflix_LoadTime.gif" alt="Carregando" srcset="" />
+    <>
+      {introNetflix == "loading" ? (
+        <div className="screen-initial">
+          <img id="gif-loading" src="https://media.filmelier.com/noticias/br/2020/03/Netflix_LoadTime.gif" alt="Carregando" srcset="" />
         </div>
+      ) : introNetflix == "intro" ? (
+        <div
+          style={{
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+            backgroundImage: `url(https://thumbs.gfycat.com/ReflectingNeglectedBug-size_restricted.gif)`,
+          }}
+          className="screen-initial"
+        ></div>
+      ) : (
+        introNetflix == "disable" && (
+          <div className="page">
+            <Header black={blackHeader} />
+            {featuredData && <FeaturedMovie item={featuredData} />}
+            <section className="lists">
+              {movieList.map((item, key) => (
+                <MovieRow key={key} title={item.title} items={item.items} />
+              ))}
+            </section>
+
+            <footer>
+              Feito por Maycon
+              <br /> Direitos de imagem para Netflix Dados pegos do site
+              <br />
+              Themoviedb.org
+            </footer>
+          </div>
+        )
       )}
-    </div>
+    </>
   );
 };
